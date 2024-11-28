0c70279fa863939fd31bf066f234231906a7f174
Zas Mooncat <zasmooncat@MacBook-Air-de-Zas.local>
2024-11-28 13:26:36 +0100

    starwars_final2

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,11 +1,10 @@
-import React, { useState, useEffect, useContext } from "react";
+import React from "react";
 import { useNavigate } from "react-router";
-import Navbar from "../component/navbar";
-import { Context } from "../store/appContext";
 import death_star from "../../img/death_star.png"
-import SwText from "./swtext";
 import 'animate.css';
 import { Link } from "react-router-dom";
+
+
 
 
 export const Home = () => {
@@ -13,12 +12,12 @@ export const Home = () => {
   return (
   
       <Link to={"/swtext"}>
+      
     <div className="deathdiv d-flex ">
       <img className="death m-auto animate__animated animate__slideInDown" src={death_star} onClick={navigate`/swtext`}/>
-      
     </div>
-      </Link>
 
+      </Link>
     
     
     
